37d7183e51897c7a585f6817dbb3154fbc223fba
Golemyte <62488182+Golemyte@users.noreply.github.com>
2023-11-15 21:18:47 +0100

    Create app.py.

---
+++ app.py
@@ -0,0 +1,8 @@
+Créez une application Python 3 qui utilise la librairie requests pour interroger un service d'API public de votre choix. Par exemple, cette application pourrait interroger le service PunAPI pour afficher un jeu de mot.
+
+Créez un nouveau repository public "ue19_labo09-10" dans votre compte GitHub. Ce repository doit contenir les fichiers suivants:
+
+README.md - description des fonctionnalités du programme, comment l'installer, le lancer, ... (howto)
+app.py - le programme qui interroge l'API
+requirements.txt - dépendances de votre programme
+Dockerfile - le fichier qui permet de créer l'image et de lancer le conteneur de app.py
